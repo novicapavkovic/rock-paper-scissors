75b8d36fd25cf7652534d9195439643ce6d2ef74
novicapavkovic <novicapavkovic@gmail.com>
2022-11-17 19:14:32 +0100

    Initial Commit

---
+++ rock paper scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock paper scissors
+//
+//  Created by Novica Pavkovic on 17.11.22..
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
